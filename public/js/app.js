a2b2948a9b2709fae91aad7bf0b7a1d2cd4d8720
andrey <andreytorrente@gmail.com>
2022-04-20 02:01:29 -0600

    update dependencies and delete gulp

--- public/js/app.js
+++
@@ -1,2 +0,0 @@
-const menuIcon=document.querySelector(".menu-icon"),menu=document.querySelector(".aside");menuIcon.addEventListener("click",()=>menu.classList.toggle("aside-active"));
-//# sourceMappingURL=app.js.map
